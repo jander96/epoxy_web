c4e4696bb3415dde9dad59f144e86cbdb1d681de
Jander Laffita <janderdeveloper@gmail.com>
2025-12-11 13:40:31 -0500

    add key

--- src/service/products.ts
+++ src/service/products.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseKey = import.meta.env.SUPABASE_ANON_KEY;
+const supabaseUrl = "https://fhukrgrbbwxpkbqquhgb.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZodWtyZ3JiYnd4cGticXF1aGdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0NzEzODgsImV4cCI6MjA4MTA0NzM4OH0.zFI0OBjiyOF_vd84peLWnvsVF9GL5ebAEAq66jTuK7Y"
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
